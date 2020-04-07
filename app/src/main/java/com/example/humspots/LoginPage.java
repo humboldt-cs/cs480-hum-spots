b4b8e09aa9d72e4cf34b037972ee68e78a388bda
breakfastlife <bpe2@humboldt.edu>
2020-04-07 11:21:54 -0700

    began to fix the branch and recreate my work as desktop github deleted a lot of my work. my local backup was about 2 hours of work behind.

--- app/src/main/java/com/example/humspots/LoginPage.java
+++ app/src/main/java/com/example/humspots/LoginPage.java
@@ -1,4 +1,13 @@
 package com.example.humspots;
 
-public class LoginPage {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginPage extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
 }
--- app/src/main/java/com/example/humspots/MainActivity.java
+++ app/src/main/java/com/example/humspots/MainActivity.java
@@ -4,10 +4,10 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class CreateNewUser extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_new_user);
+        setContentView(R.layout.activity_main);
     }
 }
