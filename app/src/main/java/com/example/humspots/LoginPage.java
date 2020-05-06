b51bae0b6c8c90435797892184ccbc4720ef9c35
breakfastlife <bpe2@humboldt.edu>
2020-05-05 19:47:23 -0700

    Having major issues with running it. AAPT: error: :style>. is thrown whenever I try to run it. Doesnt even build. I think something in my Gradle is out of date or something. However, firebase is implemented. The google sign in is not yet.

--- app/src/main/java/com/example/humspots/LoginPage.java
+++ app/src/main/java/com/example/humspots/LoginPage.java
@@ -2,48 +2,53 @@ package com.example.humspots;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.parse.LogInCallback;
-import com.parse.ParseException;
-import com.parse.ParseUser;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginPage extends AppCompatActivity {
 
     public static final String TAG = "LoginActivity";
-    private EditText etUserName;
+    private EditText etEmail;
     private EditText etPassword;
     private Button btnLogin;
     private Button btnCreate;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
         //linking the activity to this create
         btnLogin = findViewById(R.id.btnLogin);
         btnCreate = findViewById(R.id.btnCreate);
         etPassword = findViewById(R.id.etPassword);
-        etUserName = findViewById(R.id.etUserName);
+        etEmail = findViewById(R.id.etEmail);
 
         //setting on clicks
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(checkUserNameAndPassword()) {
-                    Log.i(TAG, "Logging in user: " + etUserName.getText().toString() + " Password: " + etPassword.getText().toString());
-                    String UserName = etUserName.getText().toString();
+                    Log.i(TAG, "Logging in user: " + etEmail.getText().toString() + " Password: " + etPassword.getText().toString());
+                    String Email = etEmail.getText().toString();
                     String Password = etPassword.getText().toString();
-                    LoginUser(UserName, Password);
+                    //LoginUser(Email, Password);
                 }
             }
         });
@@ -52,19 +57,81 @@ public class LoginPage extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Log.i(TAG, "Clicked create new user");
-                goToCreate();
+                createNewAccount(etEmail.getText().toString(), etPassword.getText().toString());
             }
         });
     }
 
-    private void goToCreate() {
-        Log.i(TAG, "Going to create new user!");
-        Intent i = new Intent(this, CreateNewUser.class);
-        startActivity(i);
-        finish();
+    private void updateUI(FirebaseUser user) {
+        //hideProgressDialog();
+        if (user != null) {
+           // findViewById(R.id.sign_in_button).setVisibility(View.GONE);
+           // findViewById(R.id.sign_out_and_disconnect).setVisibility(View.VISIBLE);
+            goMainActivity();
+        }
     }
 
-    private void LoginUser(String userName, String password){
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    private void createNewAccount(String email, String password) {
+        Log.i(TAG, "Creating new user!");
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(LoginPage.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    private void signIn(String email, String password) {
+        Log.d(TAG, "signIn:" + email);
+
+        // [START sign_in_with_email]
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LoginPage.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                            // [START_EXCLUDE]
+                            //checkForMultiFactorFailure(task.getException());
+                            // [END_EXCLUDE]
+                        }
+                    }
+                });
+        // [END sign_in_with_email]
+    }
+
+    /*private void LoginUser(String userName, String password){
         ParseUser.logInInBackground(userName, password, new LogInCallback() {
             @Override
             public void done(ParseUser user, ParseException e) {
@@ -79,7 +146,7 @@ public class LoginPage extends AppCompatActivity {
                 }
             }
         });
-    }
+    }*/
 
     private void goMainActivity() {
         Log.i(TAG, "Going to main!");
@@ -89,9 +156,9 @@ public class LoginPage extends AppCompatActivity {
     }
 
     private boolean checkUserNameAndPassword() {
-        if((etUserName.getText().toString().contentEquals("User Name")) || (etUserName.getText().toString().isEmpty())
+        if((etEmail.getText().toString().contentEquals("User Name")) || (etEmail.getText().toString().isEmpty())
         || (etPassword.getText().toString().contentEquals("Password")) || (etPassword.getText().toString().isEmpty())){
-            Toast.makeText(this, "Username: " + etUserName.getText().toString(), Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Username: " + etEmail.getText().toString(), Toast.LENGTH_LONG).show();
             return false;
         }
         else{
